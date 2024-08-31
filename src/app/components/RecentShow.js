0663714c955ad3b52678ab98ca21098cb749eb62
bhardwaj2612 <raghav.bhardwaj2612@gmail.com>
2024-08-31 18:44:15 -0500

    recent show

--- src/app/components/RecentShow.js
+++ src/app/components/RecentShow.js
@@ -29,36 +29,34 @@ const RecentShow = () => {
     }
     return <>
         <section>
-            <div className="w-[100%] my-10 ">
+            <div className="w-[100%] h-[100vh] flex flex-col items-center mt-[7%]">
 
-                <div className=" w-[50%] mx-auto text-center" >
-                    <h1 className="text-white text-5xl max-sm:text-lg">  Recent show made Star-studded vis StarClinch </h1>
+                <div className=" w-[50%] text-center wrap3" >
+                    <h1 className="text-white text-5xl max-sm:text-xl">  Recent show made Star-studded vis StarClinch </h1>
                 </div>
 
-                <div className="flex w-[70%] justify-center items-center mx-auto">
+                <div className="flex w-[70%] max-sm:w-[100%] my-[7%] justify-center items-center">
 
-                    <Image onClick={() => slidleft()} src={"/images/back.png"} width={70} height={70} />
+                    <Image onClick={() => slidleft()} src={"/images/back.png"} width={70} height={70} className="max-sm:hidden " />
 
-                    <div ref={slidLeft} className="rounded-tl-[50%] rounded-tr-[50%] my-[10%] wrap1 flex items-center ml-[20%] mr-3 w-[50%] bg-white overflow-x-scroll">
-                        <div className="w-[100%] flex-none">
-                            <img src={image[0]} className="w-[100%]" />
-                        </div>
-                        <div className="w-[100%] flex-none">
-                            <img src={image[2]} className="w-[100%]" />
-                        </div>
-                        <div className="w-[100%] flex-none">
-                            <img src={image[1]} className="w-[100%]" />
-                        </div>
-                        <div className="w-[100%] flex-none">
-                            <img src={image[3]} className="w-[100%]" />
-                        </div>
+                    <div ref={slidLeft} className="scrollHidden rounded-tl-[50%] rounded-tr-[50%] flex items-center ml-[20%] max-sm:ml-0  mr-3 w-[50%] bg-white overflow-x-scroll">
+
+                        <img src={image[0]} className="w-[100%] flex-none" />
+                        <img src={image[2]} className="w-[100%] flex-none" />
+                        <img src={image[1]} className="w-[100%] flex-none" />
+                        <img src={image[3]} className="w-[100%] flex-none" />
+
                     </div>
 
                     <div className="w-[20%] mr-3 max-sm:mr-1">
-                        <p className="text-xl max-sm:text-base text-[#ffffff9f]"><span className="text-3xl mr-1 max-sm:text-base text-[#fff]" >{info[increment].name}
-                        </span>For an Event hosted by xyz preformed at <span className="text-3xl mr-1 max-sm:text-base text-[#fff]">{info[increment].place}</span></p>
+                        <p className="text-xl max-sm:text-base text-[#ffffff9f]">
+                            <span className="text-3xl mr-1 max-sm:text-base text-[#fff]" >{info[increment].name}</span>
+                            For an Event hosted by xyz preformed at
+                            <span className="text-3xl ml-1 max-sm:text-base text-[#fff]">{info[increment].place}</span>
+                        </p>
                     </div>
-                    <Image src={"/images/next.png"} onClick={() => slid()} width={70} height={70} />
+
+                    <Image src={"/images/next.png"} onClick={() => slid()} width={70} height={70} className="max-sm:hidden " />
 
                 </div>
             </div>
